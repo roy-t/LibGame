645dfae5a12491c636edb831763502bac7b5153d
Roy Triesscheijn <roy-t@hotmail.com>
2023-12-30 21:23:12 +0100

    add profiler for testing purposes

---
+++ LibGame.Profiler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Xml;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using LibGame.Noise;
+
+namespace LibGame.Profiler;
+
+public static class Program
+{
+    [MemoryDiagnoser]
+    public class TestFramework
+    {
+        [Benchmark] // 12.79ns
+        public float ComputeNoise()
+        {
+            const float x = 3329.0f;
+            const float y = 7907.0f;
+            return SimplexNoise.Noise(x, y);
+        }
+
+    }
+
+
+    public static void Main(string[] _)
+    {
+        BenchmarkRunner.Run<TestFramework>();
+    }
+}
