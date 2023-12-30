49f0b58bafc12142cc6f1854d675a582fc1b3f98
Roy Triesscheijn <roy-t@hotmail.com>
2023-12-30 16:04:57 +0100

    Add simplex noise, and more vector extensions

--- LibGame.Tests/Mathematics/VectorExtensionsTests.cs
+++ LibGame.Tests/Mathematics/VectorExtensionsTests.cs
@@ -3,10 +3,10 @@
 namespace LibGame.Tests.Mathematics;
 public static class VectorExtensionsTests
 {
-    [Fact(DisplayName ="`WithZ should return a new Vector3 with the given Z component value")]
-    public static void WithZ()
+    [Fact(DisplayName ="`Expand should return a new Vector3 with the given Z component value")]
+    public static void Expand()
     {
-        EqualF(new Vector3(0.0f, 0.0f, 0.0f), new Vector2(0.0f, 0.0f).WithZ(0.0f));
-        EqualF(new Vector3(0.0f, 0.0f, 1.0f), new Vector2(0.0f, 0.0f).WithZ(1.0f));
+        EqualF(new Vector3(0.0f, 0.0f, 0.0f), new Vector2(0.0f, 0.0f).Expand(0.0f));
+        EqualF(new Vector3(0.0f, 0.0f, 1.0f), new Vector2(0.0f, 0.0f).Expand(1.0f));
     }
 }
--- LibGame/Mathematics/VectorExtensions.cs
+++ LibGame/Mathematics/VectorExtensions.cs
@@ -1,11 +1,165 @@
 ﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace LibGame.Mathematics;
 
 public static class VectorExtensions
 {
-    public static Vector3 WithZ(this Vector2 vector, float z = 0.0f)
+    public enum VectorComponent : int
+    {
+        X = 0,
+        Y = 1,
+        Z = 2,
+        W = 3,
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector4 Apply(this Vector4 v, Func<float, float> function)
+    {
+        return new Vector4
+        (
+            function(v.X),
+            function(v.Y),
+            function(v.Z),
+            function(v.W)
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 Apply(this Vector3 v, Func<float, float> function)
+    {
+        return new Vector3
+        (
+            function(v.X),
+            function(v.Y),
+            function(v.Z)
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Apply(this Vector2 v, Func<float, float> function)
+    {
+        return new Vector2
+        (
+            function(v.X),
+            function(v.Y)            
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector4 Swizzle(this Vector4 v, VectorComponent a, VectorComponent b, VectorComponent c, VectorComponent d)
+    {
+        return new Vector4
+        (
+            v[(int)a],
+            v[(int)b],
+            v[(int)c],
+            v[(int)d]
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 Swizzle(this Vector4 v, VectorComponent a, VectorComponent b, VectorComponent c)
+    {        
+        return new Vector3
+        (
+            v[(int)a],
+            v[(int)b],
+            v[(int)c]
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Swizzle(this Vector4 v, VectorComponent a, VectorComponent b)
+    {
+        return new Vector2
+        (
+            v[(int)a],
+            v[(int)b]
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 Swizzle(this Vector3 v, VectorComponent a, VectorComponent b, VectorComponent c)
+    {
+        return new Vector3
+        (
+            v[(int)a],
+            v[(int)b],
+            v[(int)c]
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Swizzle(this Vector3 v, VectorComponent a, VectorComponent b)
+    {
+        return new Vector2
+        (
+            v[(int)a],
+            v[(int)b]            
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector4 Swizzle(this Vector3 v, VectorComponent a, VectorComponent b, VectorComponent c, VectorComponent d)
+    {
+        return new Vector4
+        (
+            v[(int)a],
+            v[(int)b],
+            v[(int)c],
+            v[(int)d]
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector2 Swizzle(this Vector2 v, VectorComponent a, VectorComponent b)
+    {
+        return new Vector2
+        (
+            v[(int)a],
+            v[(int)b]            
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 Swizzle(this Vector2 v, VectorComponent a, VectorComponent b, VectorComponent c)
+    {
+        return new Vector3
+        (
+            v[(int)a],
+            v[(int)b],
+            v[(int)c]
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector4 Swizzle(this Vector2 v, VectorComponent a, VectorComponent b, VectorComponent c, VectorComponent d)
+    {
+        return new Vector4
+        (
+            v[(int)a],
+            v[(int)b],
+            v[(int)c],
+            v[(int)d]
+        );
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 Expand(this Vector2 vector, float z = 0.0f)
     {
         return new Vector3(vector, z);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector4 Expand(this Vector2 vector, float z, float w)
+    {
+        return new Vector4(vector, z, w);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector4 Expand(this Vector3 vector, float w = 0.0f)
+    {
+        return new Vector4(vector, w);
     }
 }
