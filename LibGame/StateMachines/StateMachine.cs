5767271705b8f1bd948b0f8ac00c338f6384dd1b
Roy Triesscheijn <roy-t@hotmail.com>
2024-07-26 20:06:03 +0200

    State Machines?

---
+++ LibGame/StateMachines/StateMachine.cs
@@ -0,0 +1,31 @@
+﻿namespace LibGame.StateMachines;
+
+public interface IStateMachineState
+{
+    public void Tick();
+}
+
+public sealed class EmptyStateMachineState : IStateMachineState
+{
+    public void Tick() { }
+}
+
+public sealed class StateMachine
+{
+    public StateMachine()
+    {
+        this.Current = new EmptyStateMachineState();
+    }
+
+    public IStateMachineState Current { get; private set; }
+
+    public void Transition(IStateMachineState newState)
+    {
+        this.Current = newState;
+    }
+
+    public void Tick()
+    {
+        this.Current.Tick();
+    }
+}
